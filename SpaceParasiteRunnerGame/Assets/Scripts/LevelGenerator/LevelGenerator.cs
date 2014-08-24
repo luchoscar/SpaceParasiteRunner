58b94d3a497b4267acaf49f98ca4f5251064a856
luchoscar <losh177@hotmail.com>
2014-08-24 04:23:21 -0700

    Adding random track generator

--- SpaceParasiteRunnerGame/Assets/Scripts/LevelGenerator/LevelGenerator.cs
+++ SpaceParasiteRunnerGame/Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour {
+	[SerializeField] private List<GameObject> AllPossibleSegments;
+	GameObject firstSegment;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		TrackSegment.AllPossibleSegments = AllPossibleSegments;
+
+		firstSegment = (GameObject) Instantiate(AllPossibleSegments[0], Vector3.zero, Quaternion.identity);
+
+		StartCoroutine(KillTrack());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private IEnumerator KillTrack() {
+		yield return new WaitForSeconds(5.0f);
+
+		firstSegment.transform.GetComponent<TrackSegment>().DiscardBlock();
+
 	}
 }
