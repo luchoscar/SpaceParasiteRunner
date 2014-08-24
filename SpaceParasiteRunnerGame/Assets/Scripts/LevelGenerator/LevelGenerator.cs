64bfd2199676371d680a9de0d0822f06e7fb549b
luchoscar <losh177@hotmail.com>
2014-08-24 06:02:33 -0700

    Adding test ship to test track

--- SpaceParasiteRunnerGame/Assets/Scripts/LevelGenerator/LevelGenerator.cs
+++ SpaceParasiteRunnerGame/Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -5,19 +5,25 @@ using System.Collections.Generic;
 public class LevelGenerator : MonoBehaviour {
 	[SerializeField] private List<GameObject> AllPossibleSegments;
 	GameObject firstSegment;
+	public float levelTime = 10.0f;
 
 	// Use this for initialization
 	void Awake () {
-		TrackSegment.AllPossibleSegments = AllPossibleSegments;
-
-		firstSegment = (GameObject) Instantiate(AllPossibleSegments[0], Vector3.zero, Quaternion.identity);
-
-		StartCoroutine(KillTrack());
+		InitializeLevel();
 	}
 
 	private IEnumerator KillTrack() {
 		yield return new WaitForSeconds(5.0f);
 
 		firstSegment.transform.GetComponent<TrackSegment>().DiscardBlock(2.0f);
+	}
+
+	public void InitializeLevel() {
+		TrackSegment.AllPossibleSegments = AllPossibleSegments;
+		TrackSegment.TrackDuration = levelTime;
+		TrackSegment.levelCreationTimeStamp = Time.time;
+		firstSegment = (GameObject) Instantiate(AllPossibleSegments[0], Vector3.zero, Quaternion.identity);
+		
+		//StartCoroutine(KillTrack());
 	}
 }
